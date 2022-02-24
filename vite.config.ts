1939379670530ae4bafce040cdb685f45dcd695c
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-02-24 23:29:42 +0000

    perf: unify vite and vitest

--- vite.config.ts
+++ vite.config.ts
@@ -166,10 +166,15 @@ export default defineConfig({
 
   // https://github.com/vitest-dev/vitest
   test: {
-    include: ['tests/unit/**/*.test.ts'],
-    environment: 'jsdom',
+    clearMocks: true,
     deps: {
       inline: ['@vue', '@vueuse', 'vue-demi'],
+    },
+    environment: 'jsdom',
+    globals: true,
+    include: ['tests/unit/**/*.test.ts'],
+    transformMode: {
+      web: [/\.[jt]sx$/],
     },
   },
 })
